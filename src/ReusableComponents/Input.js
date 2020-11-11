7cf9d358e478cb3827da30983555fdd67db3db71
Gokul S <gokuls2781@gmail.com>
2020-11-11 15:38:22 +0530

    commit

--- src/ReusableComponents/Input.js
+++ src/ReusableComponents/Input.js
@@ -1,16 +1,16 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core";
-const useStyles = makeStyles((theme) => ({
-  root: {
-    borderRadius: "20px",
-    background: "#f9fafa",
-    boxShadow: "inset 17px 17px 34px #e5e6e6,inset -17px -17px 34px #ffffff",
-  },
-}));
+// import { makeStyles } from "@material-ui/core";
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     borderRadius: "20px",
+//     background: "#f9fafa",
+//     boxShadow: "inset 17px 17px 34px #e5e6e6,inset -17px -17px 34px #ffffff",
+//   },
+// }));
 
 const Input = (props) => {
-  const classes = useStyles();
+  // const classes = useStyles();
   return (
     <div>
       <TextField
@@ -19,9 +19,8 @@ const Input = (props) => {
         type={props.type}
         name={props.name}
         onChange={props.changeHandler}
-        style={{ width: 250 }}
-        size="small"
-        className={classes.root}
+        size={props.size}
+        className={props.class}
       />
     </div>
   );
